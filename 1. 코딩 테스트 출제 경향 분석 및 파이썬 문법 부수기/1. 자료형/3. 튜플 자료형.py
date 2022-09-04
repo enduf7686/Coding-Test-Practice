72354a610066c7255e99856bdc586ade21df5b35
enduf7686 <enduf768640@gmail.com>
2022-09-04 17:09:38 +0900

    파이썬 문법 3

---
+++ 1. 코딩 테스트 출제 경향 분석 및 파이썬 문법 부수기/1. 자료형/3. 튜플 자료형.py
@@ -0,0 +1,20 @@
+# 문자열 자료형
+
+# 1. 문자열 초기화
+data = 'Hello World'
+print(data)
+
+data = "Don`t you know \"Python\"?" # "\"을 이용하여 문자열내에 큰 따옴표, 작은 따옴표 포함 가능
+print(data)
+
+# 2. 문자열 연산
+a = "Hello"
+b = "World"
+print(a + " " + b) # 덧셈
+
+a = "String"
+print(a * 3) # 곱셈
+
+a = "ABCDEF"
+print(a[5]) # 인덱싱
+print(a[2 : 4]) # 슬라이싱
